463c87ac4a60d1507a856c2771788ce2d58dcfdd
sudosan <admin@syaroshi.co>
2018-01-01 09:54:23 +0900

    季節イベントを追加

---
+++ picplayweb/public/javascripts/seasonal_event.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+  var dt = new Date();
+  //元旦
+  if(dt.getMonth()==0&&dt.getDay()<10){
+    document.body.classList.add('newyear');
+    document.getElementById("topinfoText").textContent="新年明けましておめでとうございます。今年もPicPlayerをよろしくお願いいたします";
+  }
+});
